b2e5a496227437b16fe88d358eb62d9985e4c6ef
gamesense31 <ritiksheoran1010@gmail.com>
2025-10-03 13:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+const student = {
+  name: "John"
+};
+
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+
+console.log(getKeys(student)); 
+
+
+const student2 = {
+  name: "Alice",
+  age: 22,
+  city: "New York"
+};
+
+console.log(getKeys(student2));
